03d088c91e704426b0551c2a371b0eb2a0647a00
Thomas Whitington <thomaswhitington@gmail.com>
2017-09-25 15:08:33 +0200

    Implemented small script for extracting tumor sample allelic fraction data from VCF and writing to a bedGraph file.

---
+++ generate_allelic_fraction_bedGraph.py
@@ -0,0 +1,28 @@
+import vcf
+import begin
+import logging
+
+class InvalidInputVcfException(Exception):
+    pass
+
+
+@begin.start(auto_convert=True)
+def extract_somatic_afs(vcf_filename, output="output.bedGraph"):
+    logging.basicConfig(level=logging.DEBUG)
+
+    vcf_reader = vcf.Reader(filename=vcf_filename)
+
+    # Extract the names of all tumor (i.e. non-normal) samples:
+    non_normal_samples = [sample for sample in vcf_reader.samples if sample.split("-")[3] != "N"]
+
+    # Currently just cope with input files containing exactly one tumor sample:
+    if len(non_normal_samples) != 1:
+        raise InvalidInputVcfException("Input VCF must include exactly one tumor (i.e. non-normal) sample.")
+
+    non_normal_sample = non_normal_samples[0]
+
+    with open(output, 'w') as output_file:
+        for record in vcf_reader:
+            tumor_genotype = record.genotype(non_normal_sample)
+            allelic_fraction = tumor_genotype.data.AO/tumor_genotype.data.DP
+            print("%s\t%d\t%d\t%1.3f" % (record.CHROM, record.POS, record.POS, allelic_fraction), file=output_file)
